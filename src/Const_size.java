13fe0afa56f9ee8af71fa9c8b8dc1fc04c00881d
Hubert Wajda <hubert.wajda03@gmail.com>
2023-03-01 00:16:45 +0100

    Revert "Adding "game over" and "pause""
    This reverts commit 6e4545518dacd81491affa87860f4ac23be9351c.

--- src/Const_size.java
+++ src/Const_size.java
@@ -2,5 +2,4 @@ final public class Const_size {
     final static public int  SCREEN_HEIGHT=500;
     final static public int SCREEN_WIDTH=700;
     final static public int UNIT_SIZE=25;
-    final static public int FONT_SIZE=75;
 }
